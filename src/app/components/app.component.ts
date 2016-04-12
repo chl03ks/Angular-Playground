9c10f2162cb97a4d46d88231220933c1d9b27c0e
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-04-11 20:32:18 -0500

    Cleaning seed to basic understanding and creating first component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,16 +1,13 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {NavbarComponent} from './navbar.component';
-import {ToolbarComponent} from './toolbar.component';
 import {HomeComponent} from '../../home/components/home.component';
 import {AboutComponent} from '../../about/components/about.component';
-import {NameListService} from '../../shared/services/name-list.service';
 
 @Component({
   selector: 'sd-app',
-  viewProviders: [NameListService],
   templateUrl: './app/components/app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES, NavbarComponent]
 })
 @RouteConfig([
   { path: '/',      name: 'Home',  component: HomeComponent  },
