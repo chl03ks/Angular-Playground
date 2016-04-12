ee5eaa18572b1ddfb6c549616608331522e4f1e2
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-04-11 20:46:48 -0500

    Multiple Components

--- src/home/components/home.component.ts
+++ src/home/components/home.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {MyComponentComponet} from './my-component/my-component.component';
 
 @Component({
-  selector: 'sd-home',
+  selector   : 'sd-home',
   templateUrl: './home/components/home.component.html',
-  styleUrls: ['./home/components/home.component.css'],
+  styleUrls  : ['./home/components/home.component.css'],
+  directives : [MyComponentComponet]
 })
 export class HomeComponent {}
---
+++ src/home/components/my-component/my-component.component.ts
@@ -0,0 +1,12 @@
+import { Component} from 'angular2/core';
+
+@Component({
+    selector   : 'sd-my-component',
+    templateUrl: './home/components/my-component/my-component.component.html',
+    directives : [],
+    providers  : [],
+    styleUrls  : ['./home/components/my-component/my-component.component.css'],
+    pipes      : []
+})
+
+export class MyComponentComponet {}
